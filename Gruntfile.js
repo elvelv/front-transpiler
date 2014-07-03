d01976bc65125639759de0e6a6f8977d30bbbb37
Yury Shapkarin <shapkarin@narr8.me>
2014-07-03 13:40:20 +0400

    new

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,32 @@ module.exports = function(grunt) {
     jade: {
         html: {
             src: 'jade/*.jade',
-            dest: 'html/test.html',
+            dest: 'output/index.html',
             options: {
                 client: false,
                 pretty: true,
-                data: grunt.file.readYAML('content/test.yaml')
+                data: grunt.file.readYAML('content/data.yaml')
             }
         }
     },
+    stylus: {
+      compile: {
+          options: {
+              paths: ['stylus/include'/*, 'another/to/import'*/], // Specifies directories to scan for @import directives when parsing.
+              /*use: [
+                  require('fluidity') // use stylus plugin at compile time
+              ],
+              import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
+                  'foo',       //  that is compiled. These might be findable based on values you gave
+                  'bar/moo'    //  to `paths`, or a plugin you added under `use`
+              ]*/
+          },
+          files: {
+              'output/style.css': 'stylus/*.styl', // 1:1 compile
+          },
+          'include css': true
+      }
+    }
   });
 
   // Load the plugin that provides the task.
